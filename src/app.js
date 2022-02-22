c6599d97f0d82615f727c095f3489f73d59586b1
vmustafa <vmustafa@hotmail.com>
2022-02-22 19:13:02 +0530

    About Text Add

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Mustafa'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Mustafa'
     })
 })
 
